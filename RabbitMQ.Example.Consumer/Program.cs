afd153e9e9cccdbb4da857bd47288eadb00ac7ec
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2026-01-23 14:53:26 +0300

    Add project files.

---
+++ RabbitMQ.Example.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Create Connection
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://zubzgupk:gqHuAcnSTkzF5gfbSkxJB4Z_oMHHSzPX@stingray.rmq.cloudamqp.com/zubzgupk");
+
+// Activate Connection - Open Channel
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+// Declare Queue
+// It have to be exactly the same with publisher
+await channel.QueueDeclareAsync(queue: "example-queue", exclusive:false);
+
+// Read Messages
+AsyncEventingBasicConsumer consumer = new(channel);
+string message = await channel.BasicConsumeAsync(queue: "example-queue", false, consumer);
+consumer.ReceivedAsync += async (sender, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    string messageText = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message: {messageText}");
+
+    // Message handled
+    await channel.BasicAckAsync(ea.DeliveryTag, multiple: false);
+};
---
+++ RabbitMQ.Example.Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Create Connection
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://zubzgupk:gqHuAcnSTkzF5gfbSkxJB4Z_oMHHSzPX@stingray.rmq.cloudamqp.com/zubzgupk");
+
+// Activate Connection - Open Channel
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+// Declare Queue
+await channel.QueueDeclareAsync(queue: "example-queue", exclusive:false);
+
+// Publish Messages
+// RabbitMQ message body is byte array
+
+byte[] message = Encoding.UTF8.GetBytes("Hello from RabbitMQ Example Publisher!");
+await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message);
+
+Console.Read();
