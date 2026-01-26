d3b08a2f1ce2e6dc82dcf882f28ccf6b14d1ffd2
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2026-01-26 18:20:33 +0300

    Added: Topic Exchange

---
+++ RabbitMQ.Topic.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using static RabbitMQ.Shared.Helpers.RabbitMQService;
+
+var (channel, connection) = await CreateChannel();
+
+using (connection) using (channel)
+{
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "topic_logs",
+        type: ExchangeType.Topic);
+
+    QueueDeclareOk queue = await channel.QueueDeclareAsync(exclusive: false);
+    string queueName = queue.QueueName;
+
+    Console.Write("Enter topic: ");
+    string topic = string.Empty;
+    topic = Console.ReadLine();
+
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "topic-example-exchange",
+        routingKey: topic);
+
+    AsyncEventingBasicConsumer consumer = new(channel);
+
+    await channel.BasicConsumeAsync(
+        queue: queueName,
+        autoAck: true,
+        consumer: consumer);
+
+    consumer.ReceivedAsync += async (sender, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.Span);
+        Console.WriteLine($"Message: {message}");
+    };
+
+
+    Console.ReadLine();
+}
---
+++ RabbitMQ.Topic.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using static RabbitMQ.Shared.Helpers.RabbitMQService;
+
+var (channel, connection) = await CreateChannel();
+
+using (connection) using (channel)
+{
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "topic-example-exchange",
+        type: ExchangeType.Topic);
+
+
+    for (int i = 0; i < 100; i++)
+    {
+        string topic = string.Empty;
+        Console.Write("Enter Topic : ");
+        topic = Console.ReadLine();
+
+        string message = $"Message {i} from Topic Exchange publisher";
+        byte[] body = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(
+            exchange: "topic-example-exchange",
+            routingKey: topic,
+            body: body);
+    }
+
+    Console.ReadLine();
+}
