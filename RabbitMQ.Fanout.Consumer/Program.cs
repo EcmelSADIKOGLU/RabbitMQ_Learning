9c6b48bb88aaa2c0e74bcf792d11bee7065fa3bf
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2026-01-26 17:47:31 +0300

    Added: Fanout exchange example

---
+++ RabbitMQ.Fanout.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using static RabbitMQ.Shared.Helpers.RabbitMQService;
+
+var (channel, connection) = await CreateChannel();
+
+using (connection) using (channel)
+{
+    //Declare Exchange
+    // It must be exactly the same with publisher
+    await channel.ExchangeDeclareAsync(
+        exchange: "fanout-example-exchange",
+        type: ExchangeType.Fanout);
+
+    // Declare Queue
+    var queue = await channel.QueueDeclareAsync(exclusive: false); // Let RabbitMQ create a random queue name
+    var queueName = queue.QueueName;
+
+    // Bind Queue to Exchange
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "fanout-example-exchange",
+        routingKey: "ignored"); // Routing Key is ignored for Fanout Exchange
+
+    //Create Consumer
+    AsyncEventingBasicConsumer consumer = new(channel);
+    
+    await channel.BasicConsumeAsync(
+        queue: queueName,
+        autoAck: true,
+        consumer: consumer);
+
+    // When Message Received trigger fonction
+    consumer.ReceivedAsync += async (sender, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.Span);
+        Console.WriteLine($"Message Received: {message}");
+    };
+
+    Console.Read();
+}
---
+++ RabbitMQ.Fanout.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using static RabbitMQ.Shared.Helpers.RabbitMQService;
+
+var (channel, connection) = await CreateChannel();
+
+using (connection) using (channel)
+{
+    //Declare Exchange
+    await channel.ExchangeDeclareAsync(
+        exchange: "fanout-example-exchange",
+        type: ExchangeType.Fanout);
+
+    for (int i = 0; i < 100; i++)
+    {
+        await Task.Delay(200);
+
+        string message = $"Hello Fanout Exchange! Message Number: {i}";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        //Publish Message to Exchange
+        await channel.BasicPublishAsync(
+            exchange: "fanout-example-exchange",
+            routingKey: "not important", // Routing Key is ignored for Fanout Exchange
+            body: body);
+
+        Console.WriteLine($"Message Sent: {message}");
+    }
+
+    Console.Read();
+}
