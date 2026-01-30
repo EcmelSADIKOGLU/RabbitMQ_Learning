75bb8d77d0966e3bdca83bf6657eeb63de3c2e74
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2026-01-30 23:09:54 +0300

    Created: Masstransit Projects

---
+++ RabbitMQ.Masstransit.Bus/Messages/ExampleMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Masstransit.Bus.Messages
+{
+    public class ExampleMessage : IMessage
+    {
+        public string Text { get; set; }
+
+    }
+}
---
+++ RabbitMQ.Masstransit.Bus/Messages/IMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Masstransit.Bus.Messages
+{
+    public interface IMessage
+    {
+        public string Text { get; set; }
+    }
+}
