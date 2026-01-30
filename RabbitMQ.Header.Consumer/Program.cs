20e1e4f61542c62916eb0b0b2da0c495b9088bb2
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2026-01-30 22:24:41 +0300

    Added: Header Exchange

---
+++ RabbitMQ.Header.Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using static RabbitMQ.Shared.Helpers.RabbitMQService;
+
+var (channel, connection) = await CreateChannel();
+
+using (connection) using (channel)
+{
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "header-example-exchange",
+        type: ExchangeType.Headers);
+
+    QueueDeclareOk queue = await channel.QueueDeclareAsync(exclusive: false);
+    string queueName = queue.QueueName;
+
+
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "header-example-exchange",
+        routingKey: string.Empty,
+
+        arguments: new Dictionary<string, object?>
+        {
+            ["x-match"] = "all", // "any" for OR logic
+            ["format"] = "pdf",
+            ["shape"] = "a4"
+        });
+
+    AsyncEventingBasicConsumer consumer = new(channel);
+
+    await channel.BasicConsumeAsync(
+        queue: queueName,
+        autoAck: true,
+        consumer: consumer);
+
+    consumer.ReceivedAsync += async (sender, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.Span);
+        Console.WriteLine($"Message: {message}");
+    };
+
+
+    Console.ReadLine();
+}
---
+++ RabbitMQ.Header.Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using static RabbitMQ.Shared.Helpers.RabbitMQService;
+
+var (channel, connection) = await CreateChannel();
+
+using (connection) using (channel)
+{
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "header-example-exchange",
+        type: ExchangeType.Headers);
+
+
+    for (int i = 0; i < 100; i++)
+    {
+        await Task.Delay(200);
+
+        string message = $"Message {i} from Header Exchange publisher";
+        byte[] body = Encoding.UTF8.GetBytes(message);
+
+
+        
+        await channel.BasicPublishAsync(
+            exchange: "header-example-exchange",
+            routingKey: string.Empty,
+            mandatory: false,
+            basicProperties: new BasicProperties
+            {
+                Headers = new Dictionary<string, object?>
+                {
+                    ["format"] = "pdf",
+                    ["shape"] = "a4"
+                }
+            },
+            body: body);
+    }
+
+    Console.ReadLine();
+}
